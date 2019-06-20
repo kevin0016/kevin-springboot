c157c589a6ffac5262437630d7212de79a529d09
梁学凯 <liangxuekai@koolearn.com>
2019-06-20 14:55:48 +0800

    测试hooks

--- configtest/src/main/java/com/itkevin/springcloud/configtest/ConfigtestApplication.java
+++ configtest/src/main/java/com/itkevin/springcloud/configtest/ConfigtestApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 public class ConfigtestApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(ConfigtestApplication.class, args);
     }
